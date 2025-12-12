201ce2775c1387d6bcd9433082dfa3637a28b008
Safa <tsabitahsafa06@gmail.com>
2025-12-12 15:17:06 +0700

    Make responsive website

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const navbar = document.getElementsByTagName('nav')[0];
 const logo = document.getElementById('logo-sida');
 
 window.addEventListener('scroll', function(){
-console.log(logo.src);
     if(window.scrollY > 1){
+        console.log(this.window.scrollY);
         navbar.classList.replace('bg-transparent', 'nav-color');
         navbar.classList.remove('navbar-dark');
         logo.src = 'assets/logo/logo-color.svg';
@@ -13,7 +13,13 @@ console.log(logo.src);
         navbar.classList.add('navbar-dark');
         logo.src = 'assets/logo/logo-white.svg';
     }
-})
+});
+
+function offcanvasAdaptation() {
+    if (window.innerWidth > 991) {
+        
+    }
+}
 
 // ============================== Grup Bisnis ==============================
 // // Mendapatkan elemen
@@ -75,6 +81,58 @@ console.log(logo.src);
 //         grupBisnisDeskripsi.style.opacity = 1;
 //     }, 500);
 // }
+
+// ============================== Section Keunggulan ==============================
+let children = [];
+
+function removeParent() {
+    const parent = document.getElementById('keunggulan-card-group-to-hide');
+    const container = document.getElementById('keunggulan-wrapper');
+
+    if (parent && container) {
+        while (parent.firstChild) {
+            children.push(parent.firstChild);
+            container.insertBefore(parent.firstChild, parent);
+        }
+        
+        parent.remove();
+    }
+}
+
+function addParent() {
+    const container = document.getElementById('keunggulan-wrapper');
+    
+    if (container && children.length > 0) {
+        const parent = document.createElement('div');
+        parent.id = 'keunggulan-card-group-to-hide';
+        
+        container.appendChild(parent);
+        
+        children.forEach(child => {
+            parent.appendChild(child);
+        });
+        
+        children = [];
+    }
+}
+
+function checkScreenSize() {
+    const parentExists = document.getElementById('keunggulan-card-group-to-hide');
+    
+    if (window.innerWidth > 575) {
+        if (parentExists) {
+            removeParent();
+        }
+    } else {
+        if (!parentExists) {
+            addParent();
+        }
+    }
+}
+
+// Panggil fungsi saat halaman dimuat dan saat window di-resize
+window.addEventListener('load', checkScreenSize);
+window.addEventListener('resize', checkScreenSize);
 
 // ============================== Portfolio Carousel ==============================
 const portfolioWrapper = document.querySelector(".portfolio-wrapper");
