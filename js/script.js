6b689c9c124465b9431b6360d3632607b8f8d533
Safa <tsabitahsafa06@gmail.com>
2026-03-10 14:56:24 +0700

    Finish revisi grup bisnis

--- js/script.js
+++ js/script.js
@@ -47,19 +47,17 @@ async function loadData() {
     }
 }
 
-// Grup Bisnis Button
 function createButtons() {
     const subBisinsButton = document.getElementById('switch-button-grup-bisnis');
+
     data.subBusinesses.forEach((sub, index) => {
         const button = document.createElement('button');
         button.classList.add('card-layout', 'd-sm-flex', 'd-block');
         
-        // span for tag
         const span = document.createElement('span');
         span.classList.add('primary-tag');
         span.textContent = sub.tag;
         
-        // h6 for sub business title
         const h6 = document.createElement('h6');
         h6.classList.add('heading-6', 'mb-0', 'mt-sm-0', 'mt-2');
         h6.textContent = sub.name;
@@ -75,10 +73,12 @@ function createButtons() {
             updateActiveButton(index);
         });
         
-        // Tambahkan button ke container
         subBisinsButton.appendChild(button);
-        buttons.push(button); // Simpan referensi tombol
+        buttons.push(button);
     });
+
+    // aktifkan button pertama saat load
+    updateActiveButton(currentIndex);
 }
 
 // Grup Bisnis Content
@@ -88,16 +88,33 @@ function renderSubBusiness(index) {
     // Title & Description
     var subBisnisTitle = document.getElementById('sub-bisnis-title');
     var subBisnisDescription = document.getElementById('sub-bisnis-desc');
-    
+
+    // reset animasi
+    subBisnisTitle.classList.remove("anim-title");
+    subBisnisDescription.classList.remove("anim-desc");
+    void subBisnisTitle.offsetWidth; // force reflow supaya animasi bisa diputar lagi
+
     subBisnisTitle.innerHTML = sub.name;
     subBisnisDescription.innerHTML = sub.description;
+
+    // trigger animasi
+    subBisnisTitle.classList.add("anim-title");
+    subBisnisDescription.classList.add("anim-desc");
     
-    // Update background image berdasarkan imageUrl dari JSON
+    // Content Image
     const judulSectionGrupBisnis = document.querySelector('.judul-section-grup-bisnis');
-    judulSectionGrupBisnis.classList.add('blur-active');
-    if (sub.imageUrl) {
-        judulSectionGrupBisnis.style.background = `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 59.18%, #FFF 93.64%), url(${sub.imageUrl}) lightgray 50% / cover no-repeat`;
-    }
+    judulSectionGrupBisnis.classList.add('fade-out');
+
+    judulSectionGrupBisnis.addEventListener('transitionend', function handler() {
+
+        if (sub.imageUrl) {
+            judulSectionGrupBisnis.style.setProperty('--bg-image', `url(${sub.imageUrl})`);
+        }
+
+        judulSectionGrupBisnis.classList.remove('fade-out');
+
+        judulSectionGrupBisnis.removeEventListener('transitionend', handler);
+    });
     
     // Fokus Bisnis
     var focusBusiness = sub.focusBusiness; 
